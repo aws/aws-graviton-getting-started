8d19c2827e6933a8400bcb756f980b2cd88e8e3e
Geoff Blake <blakgeof@amazon.com>
2021-09-17 09:33:37 -0500

    perfrunbook: Fix typos in scripts, and improve dependency install script to recognize all minor versions of Ubuntu 20.04 LTS.

--- perfrunbook/utilities/configure_vcpus.sh
+++ perfrunbook/utilities/configure_vcpus.sh
@@ -26,18 +26,21 @@ for i in $all_vcpus; do
 done
 
 # Parse lscpu output to select the subset of cores/threads to hotplug in/out
-if [[ "$2" == "threads" ]]; then
-  let physical_cores=$1/2
-  vcpus_on=$(lscpu -p=CPU,CORE | awk -F',' 'BEGIN{j='"${physical_cores}"'}$0 ~ /^[[:digit:]]+,[[:digit:]]+$/{if ($2<j) {print $1}; i++}')
-  vcpus_off=$(lscpu -p=CPU,CORE | awk -F',' 'BEGIN{j='"${physical_cores}"'}$0 ~ /^[[:digit:]]+,[[:digit:]]+$/{if ($2>=j) {print $1}; i++}')
-elif [[ "$2" == "cores" ]]; then
-  let physical_cores=$1
-  vcpus_on=$(lscpu -p=CPU,CORE | awk -F',' 'BEGIN{j='"${physical_cores}"'}$0 ~ /^[[:digit:]]+,[[:digit:]]+$/{if ($1<j) {print $1}; i++}')
-  vcpus_off=$(lscpu -p=CPU,CORE | awk -F',' 'BEGIN{j='"${physical_cores}"'}$0 ~ /^[[:digit:]]+,[[:digit:]]+$/{if ($1>=j) {print $1}; i++}')
-else
-  echo "Do not recognize option: $2"
-  exit 1
-fi
+case $2 in
+  threads)
+    let physical_cores=$1/2
+    vcpus_on=$(lscpu -p=CPU,CORE | awk -F',' 'BEGIN{j='"${physical_cores}"'}$0 ~ /^[[:digit:]]+,[[:digit:]]+$/{if ($2<j) {print $1}; i++}')
+    vcpus_off=$(lscpu -p=CPU,CORE | awk -F',' 'BEGIN{j='"${physical_cores}"'}$0 ~ /^[[:digit:]]+,[[:digit:]]+$/{if ($2>=j) {print $1}; i++}')
+    ;;
+  cores)
+    let physical_cores=$1
+    vcpus_on=$(lscpu -p=CPU,CORE | awk -F',' 'BEGIN{j='"${physical_cores}"'}$0 ~ /^[[:digit:]]+,[[:digit:]]+$/{if ($1<j) {print $1}; i++}')
+    vcpus_off=$(lscpu -p=CPU,CORE | awk -F',' 'BEGIN{j='"${physical_cores}"'}$0 ~ /^[[:digit:]]+,[[:digit:]]+$/{if ($1>=j) {print $1}; i++}')
+    ;;
+  *)
+    echo "Do not recognize option: $2"
+    exit 1
+esac
 
 for i in $vcpus_on; do
   echo 1 > /sys/devices/system/cpu/cpu$i/online
