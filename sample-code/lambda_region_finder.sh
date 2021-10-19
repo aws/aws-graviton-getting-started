118d125076c2d30aa97a3ad2e5cfaeb0364b8796
“Brian <bmaguir@amazon.com>
2021-10-18 20:06:55 -0400

    comment update and simplified the script

--- sample-code/lambda_region_finder.sh
+++ sample-code/lambda_region_finder.sh
@@ -3,55 +3,34 @@
 # Graviton2 Function Finder
 # Identify Lambda functions with Graviton2 compatiable and not-ompatiable runtimes versions.  Looks in all regions where Graviton2 Lambda is currently available.
 # Lambda runtimes support for Graviton2 docs: https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html
-# This script can be easily copy and pasted into a AWS CloudShell https://aws.amazon.com/cloudshell/ shell
 
+supported_regions=(us-east-1 us-east-2 us-west-2 eu-west-1 eu-west-2 eu-central-1 ap-southeast-1 ap-southeast-2 ap-northeast-1 ap-south-1)
+supported_runtimes=(python3.8 python3.9 nodejs12.x nodejs14.x dotnetcore3.1 ruby2.7 java8.al2 java11 provided.al2)
+unsupported_runtimes=(python3.6 python3.7 python2.7 nodejs10.x dotnetcore2.1 ruby2.5 java8 go1.x provided)
 
 echo "Graviton2 Function Support Finder"
 
-for region in us-east-1 us-east-2 us-west-2 eu-west-1 eu-west-2 eu-central-1 ap-southeast-1 ap-southeast-2 ap-northeast-1 ap-south-1
+for region in "${supported_regions[@]}"
 do
     echo "  "
     echo "Region: [${region}] - Functions WITH Graviton Compatible Runtimes"
     echo "  "
 
-    # python
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='python3.8'].{ARN:FunctionArn, Runtime:Runtime}"
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='python3.9'].{ARN:FunctionArn, Runtime:Runtime}"
-    # nodejs
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='nodejs12.x'].{ARN:FunctionArn, Runtime:Runtime}"
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='nodejs14.x'].{ARN:FunctionArn, Runtime:Runtime}"
-    # dotnet
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='dotnetcore3.1'].{ARN:FunctionArn, Runtime:Runtime}"
-    # ruby
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='ruby2.7'].{ARN:FunctionArn, Runtime:Runtime}"
-    # java
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='java8.al2'].{ARN:FunctionArn, Runtime:Runtime}"
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='java11'].{ARN:FunctionArn, Runtime:Runtime}"
-    # custom
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='provided.al2'].{ARN:FunctionArn, Runtime:Runtime}"
-    # container image
+    for runtime in "${supported_runtimes[@]}"
+    do
+            aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='${runtime}'].{ARN:FunctionArn, Runtime:Runtime}"
+    
+    # include the container image functions
     aws lambda list-functions --region ${region} --output text --query "Functions[?PackageType=='Image'].{ARN:FunctionArn, PackageType:'container-image'}"
-
+    done
+    
     echo "  "
     echo "Region: [${region}] - Functions with Runtimes that are NOT Compatible with Graviton2. Require a Runtime version update."
     echo "  "
 
-    # python
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='python3.6'].{ARN:FunctionArn, Runtime:Runtime}"
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='python3.7'].{ARN:FunctionArn, Runtime:Runtime}"
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='python2.7'].{ARN:FunctionArn, Runtime:Runtime}"
-    # nodejs
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='nodejs10.x'].{ARN:FunctionArn, Runtime:Runtime}"
-    # dotnet
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='dotnetcore2.1'].{ARN:FunctionArn, Runtime:Runtime}"
-    # ruby
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='ruby2.5'].{ARN:FunctionArn, Runtime:Runtime}"
-    # java
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='java8'].{ARN:FunctionArn, Runtime:Runtime}"
-    # go
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='go1.x'].{ARN:FunctionArn, Runtime:Runtime}"
-    # custom
-    aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='provided'].{ARN:FunctionArn, Runtime:'Runtime'}"
-
+    for runtime in "${unsupported_runtimes[@]}"
+    do
+            aws lambda list-functions --region ${region} --output text --query "Functions[?Runtime=='${runtime}'].{ARN:FunctionArn, Runtime:Runtime}"
+    done
 done
 echo "finished"
