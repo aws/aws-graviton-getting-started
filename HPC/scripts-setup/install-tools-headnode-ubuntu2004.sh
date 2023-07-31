4ad66ae8541937464eeacb4eb51ea692621f3a20
DaisukeMiyamoto <midaisuk@amazon.co.jp>
2023-07-31 17:02:41 +0000

    add installing GCC11 commands to install-tools-headnode-ubuntu2004.sh script

--- HPC/scripts-setup/install-tools-headnode-ubuntu2004.sh
+++ HPC/scripts-setup/install-tools-headnode-ubuntu2004.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# install GCC11 and use alternatives
+sudo apt update -y
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt install -y gcc-11 g++-11 gfortran-11
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11 --slave /usr/bin/gfortran gfortran /usr/bin/gfortran-11
+
 # download acfl for ubuntu 20.04 from arm website - https://developer.arm.com/downloads/-/arm-compiler-for-linux
 # install acfl will include armpl automatically
 mkdir -p /shared/tools
